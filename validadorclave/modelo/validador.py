2a7f56c1209a98ddb97ccdf93a29733e7b54bce1
Samuel GG <samuelgilest@gmail.com>
2024-10-16 12:36:15 -0500

    _validar_longitud method in class ReglaValidacion defined

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -6,3 +6,9 @@ class ReglaValidacion(ABC):
     def __init__(self, _longitud_esperada: int):
         self._longitud_esperada: int = _longitud_esperada
 
+    def _validar_longitud(self, clave: str) -> bool:
+        if len(clave) >= self._longitud_esperada:
+            return True
+        else:
+            return False
+
