85b878dbe266bd9dc435f031d56cd4cfbd1e20f0
Samuel GG <samuelgilest@gmail.com>
2024-10-16 12:40:49 -0500

    _contiene_mayuscula, _contiene_minuscula and _contiene_numero methods in class ReglaValidacion defined

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -12,3 +12,20 @@ class ReglaValidacion(ABC):
         else:
             return False
 
+    def _contiene_mayuscula(self, clave: str) -> bool:
+        if clave.isupper():
+            return True
+        else:
+            return False
+
+    def _contiene_minuscula(self, clave: str) -> bool:
+        if clave.islower():
+            return True
+        else:
+            return False
+
+    def _contiene_numero(self, clave: str) -> bool:
+        if clave.isdigit():
+            return True
+        else:
+            return False
