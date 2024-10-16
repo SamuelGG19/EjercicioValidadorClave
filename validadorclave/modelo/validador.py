f6d92a1010542a5facbdf8d35b35c0228a748643
Samuel GG <samuelgilest@gmail.com>
2024-10-16 12:30:51 -0500

    __init__ method in class ReglaValidacion defined

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,8 @@
-# TODO: Implementa el código del ejercicio aquí
+from abc import ABC, abstractmethod
+
+
+class ReglaValidacion(ABC):
+
+    def __init__(self, _longitud_esperada: int):
+        self._longitud_esperada: int = _longitud_esperada
+
